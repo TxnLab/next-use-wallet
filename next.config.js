0166abb85d4caf01ac7048df403064c5e58c6d4b
Doug Richar <drichar@gmail.com>
2023-06-21 00:08:15 -0400

    feat: use-wallet v2.0.0-alpha.0, WalletConnect 2.0

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // @see https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1487801131
+    config.externals.push('pino-pretty', 'lokijs')
+    return config
+  },
   reactStrictMode: true
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,7 @@
 import { Inter } from '@next/font/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {
-  reconnectProviders,
-  initializeProviders,
-  WalletProvider,
-  WalletClient
-} from '@txnlab/use-wallet'
-import { useEffect, useState } from 'react'
+import { WalletProvider, useInitializeProviders, PROVIDER_ID } from '@txnlab/use-wallet'
 import Toaster from 'components/Toaster'
-import { NODE_NETWORK, NODE_PORT, NODE_TOKEN, NODE_URL } from 'constants/env'
 import type { AppProps } from 'next/app'
 
 import 'styles/globals.css'
@@ -18,26 +11,27 @@ const inter = Inter({ subsets: ['latin'] })
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  const [walletProviders, setWalletProviders] = useState<Record<
-    string,
-    Promise<WalletClient | null>
-  > | null>(null)
-
-  useEffect(() => {
-    async function initialize() {
-      const providers = await initializeProviders([], {
-        network: NODE_NETWORK,
-        nodeServer: NODE_URL,
-        nodeToken: NODE_TOKEN,
-        nodePort: NODE_PORT
-      })
-
-      setWalletProviders(providers)
-      reconnectProviders(providers)
-    }
-
-    initialize()
-  }, [])
+  const walletProviders = useInitializeProviders({
+    providers: [
+      PROVIDER_ID.DEFLY,
+      PROVIDER_ID.PERA,
+      PROVIDER_ID.DAFFI,
+      PROVIDER_ID.EXODUS,
+      {
+        id: PROVIDER_ID.WALLETCONNECT,
+        clientOptions: {
+          projectId: process.env.NEXT_PUBLIC_WC2_PROJECT_ID,
+          relayUrl: process.env.NEXT_PUBLIC_WC2_RELAY_URL,
+          metadata: {
+            name: 'next-use-wallet',
+            description: 'Next.js @txnlab/use-wallet example',
+            url: 'https://next-use-wallet.vercel.app/',
+            icons: ['https://next-use-wallet.vercel.app/nfd.svg']
+          }
+        }
+      }
+    ]
+  })
 
   return (
     <>
