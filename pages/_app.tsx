f370b37f023e4cf6158c602d168f084d332cf656
Doug Richar <drichar@gmail.com>
2023-06-21 20:49:24 -0400

    fix: set nodeConfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WalletProvider, useInitializeProviders, PROVIDER_ID } from '@txnlab/use-wallet'
 import Toaster from 'components/Toaster'
+import { NODE_NETWORK, NODE_PORT, NODE_TOKEN, NODE_URL } from 'constants/env'
 import type { AppProps } from 'next/app'
 
 import 'styles/globals.css'
@@ -30,7 +31,13 @@ export default function App({ Component, pageProps, router }: AppProps) {
           }
         }
       }
-    ]
+    ],
+    nodeConfig: {
+      network: NODE_NETWORK,
+      nodeServer: NODE_URL,
+      nodePort: NODE_PORT,
+      nodeToken: NODE_TOKEN
+    }
   })
 
   return (
