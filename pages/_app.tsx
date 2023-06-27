2dbab7b3624c6c9a477d0da3ef5222ad55c0eb04
Doug Richar <drichar@gmail.com>
2023-06-27 18:39:54 -0400

    use-wallet v2.0.0, WalletConnect 2.0 (#25)
    * feat: use-wallet v2.0.0-alpha.0, WalletConnect 2.0
    
    * chore(deps): add WalletConnect 2.0 dependencies
    
    * chore(deps): @txnlab/use-wallet v2.0.0-alpha.1
    
    * fix: set nodeConfig
    
    * chore(deps): update @txnlab/use-wallet to 2.0.0-alpha.3
    
    * chore(deps): update @txnlab/use-wallet to 2.0.0-alpha.4
    
    * chore(deps): update @txnlab/use-wallet to 2.0.0-alpha.5
    
    * feat: show Fireblocks desktop wallet in WCv2 provider
    
    * chore(deps): update @txnlab/use-wallet to 2.0.0-rc.1
    
    * chore(deps): update @txnlab/use-wallet to 2.0.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
+import { DeflyWalletConnect } from '@blockshake/defly-connect'
+import { DaffiWalletConnect } from '@daffiwallet/connect'
 import { Inter } from '@next/font/google'
+import { PeraWalletConnect } from '@perawallet/connect'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {
-  reconnectProviders,
-  initializeProviders,
-  WalletProvider,
-  WalletClient
-} from '@txnlab/use-wallet'
-import { useEffect, useState } from 'react'
+import { WalletProvider, useInitializeProviders, PROVIDER_ID } from '@txnlab/use-wallet'
+import { WalletConnectModalSign } from '@walletconnect/modal-sign-html'
+import algosdk from 'algosdk'
 import Toaster from 'components/Toaster'
 import { NODE_NETWORK, NODE_PORT, NODE_TOKEN, NODE_URL } from 'constants/env'
 import type { AppProps } from 'next/app'
@@ -18,26 +17,42 @@ const inter = Inter({ subsets: ['latin'] })
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  const [walletProviders, setWalletProviders] = useState<Record<
-    string,
-    Promise<WalletClient | null>
-  > | null>(null)
-
-  useEffect(() => {
-    async function initialize() {
-      const providers = await initializeProviders([], {
-        network: NODE_NETWORK,
-        nodeServer: NODE_URL,
-        nodeToken: NODE_TOKEN,
-        nodePort: NODE_PORT
-      })
-
-      setWalletProviders(providers)
-      reconnectProviders(providers)
-    }
-
-    initialize()
-  }, [])
+  const walletProviders = useInitializeProviders({
+    providers: [
+      { id: PROVIDER_ID.DEFLY, clientStatic: DeflyWalletConnect },
+      { id: PROVIDER_ID.PERA, clientStatic: PeraWalletConnect },
+      { id: PROVIDER_ID.DAFFI, clientStatic: DaffiWalletConnect },
+      { id: PROVIDER_ID.EXODUS },
+      {
+        id: PROVIDER_ID.WALLETCONNECT,
+        clientStatic: WalletConnectModalSign,
+        clientOptions: {
+          projectId: process.env.NEXT_PUBLIC_WC2_PROJECT_ID || '',
+          relayUrl: process.env.NEXT_PUBLIC_WC2_RELAY_URL,
+          metadata: {
+            name: 'next-use-wallet',
+            description: 'Next.js @txnlab/use-wallet example',
+            url: 'https://next-use-wallet.vercel.app/',
+            icons: ['https://next-use-wallet.vercel.app/nfd.svg']
+          },
+          modalOptions: {
+            explorerRecommendedWalletIds: [
+              // Fireblocks desktop wallet
+              '5864e2ced7c293ed18ac35e0db085c09ed567d67346ccb6f58a0327a75137489'
+            ]
+          }
+        }
+      }
+    ],
+    nodeConfig: {
+      network: NODE_NETWORK,
+      nodeServer: NODE_URL,
+      nodePort: NODE_PORT,
+      nodeToken: NODE_TOKEN
+    },
+    algosdkStatic: algosdk,
+    debug: true
+  })
 
   return (
     <>
